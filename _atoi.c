fa4203c291ac6361eba176bcca8bf8ce070124fb
Simbarashe Lungu <visualnerds96.com>
2023-07-23 05:09:05 -0700

    simple_shell

---
+++ _atoi.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * interactive - if shell is interactive mode return true
+ * @info: address
+ *
+ * Return: 1 if true, 0 else
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - delimeter checker
+ * @c: char cgaracter for checking
+ * @delim: string delimeter
+ * Return: 1 true, 0 false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - alphabetic character checker
+ *@c: Character input
+ *Return: 1 true, 0 else
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - for converting string values to integer
+ *@s: string needed to be converted.
+ *Return: 0 no numbers, numbers to be changed else
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
+
